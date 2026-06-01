27e6c32a17d7918bd000f2a0f919ce6a606c36fd
AB <a.bierler@xdev-software.de>
2026-06-01 09:27:37 +0200

    Remove validation api

--- advanced-demo/entities/src/main/java/software/xdev/tci/demo/entities/Product.java
+++ advanced-demo/entities/src/main/java/software/xdev/tci/demo/entities/Product.java
@@ -1,18 +1,16 @@
 package software.xdev.tci.demo.entities;
 
+import java.util.Objects;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 
 @Entity
 @Table(name = "product")
 public class Product extends IdentifiableEntity
 {
-	@NotNull
-	@Size(max = 255)
 	@Column(name = "name", nullable = false, unique = true)
 	private String name;
 	
@@ -22,7 +20,7 @@ public class Product extends IdentifiableEntity
 	
 	public Product(final String name)
 	{
-		this.name = name;
+		this.setName(name);
 	}
 	
 	public String getName()
@@ -30,8 +28,14 @@ public class Product extends IdentifiableEntity
 		return this.name;
 	}
 	
+	@SuppressWarnings("checkstyle:MagicNumber")
 	public void setName(final String name)
 	{
+		Objects.requireNonNull(name);
+		if(name.length() > 255)
+		{
+			throw new IllegalArgumentException("name too long");
+		}
 		this.name = name;
 	}
 }
