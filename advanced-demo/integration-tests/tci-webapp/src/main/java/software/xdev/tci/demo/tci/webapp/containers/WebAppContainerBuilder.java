cbfb7e5e13c7bfe66b142d09e7c9a43001075796
AB <a.bierler@xdev-software.de>
2026-07-09 17:07:06 +0200

    Demo: Use AOT
    Should improve image startup speed

--- advanced-demo/integration-tests/tci-webapp/src/main/java/software/xdev/tci/demo/tci/webapp/containers/WebAppContainerBuilder.java
+++ advanced-demo/integration-tests/tci-webapp/src/main/java/software/xdev/tci/demo/tci/webapp/containers/WebAppContainerBuilder.java
@@ -29,66 +29,67 @@ public final class WebAppContainerBuilder
 		LOG.info("Building WebApp-DockerImage...");
 		
 		final NativeAdvancedImageFromDockerfile builder = ImageCreator.nativeImage("tci-demo-webapp")
-				.withDockerFilePath(Paths.get("../../integration-tests/tci-webapp/Dockerfile"))
-				.withBaseDir(Paths.get("../../"))
-				.configureFilesToTransferHandler(h -> h
-					.withPostGitIgnoreLines(
-						// Ignore git-folder, as it will be provided in the Dockerfile
-						".git/**",
-						// Ignore other unused folders and extensions
-						"*.iml",
-						"*.cmd",
-						"*.md",
-						"_dev_infra/**",
-						"_resource_metrics/**",
-						// Ignore other Dockerfiles (our required file will always be transferred)
-						"Dockerfile",
-						// Ignore not required test-modules that may have changed
-						// sources only - otherwise the parent pom doesn't find the resources
-						"integration-tests/**",
-						"**/src/test/**",
-						// Ignore resources that are just used for development
-						"webapp/src/main/resources-dev/**",
-						// Most files from these folders need to be ignored -> Down there for highest prio
-						"node_modules",
-						"target")
-					// File is in root directory - we can't access it
-					.withBaseDirRelativeIgnoreFile(null)
-					.withDockerFileLinesModifier(new DockerfileCOPYParentsEmulator())
-					.withTransferArchiveTARCompressorCustomizer(c -> c
-						// Rewrite parent pom to exclude integration tests
-						// This way changes in test pom's cause no redownload of dependencies
-						.withContentModifier(new FileLinesContentModifier()
+			.withBuildArg("ENABLE_AOT", "1")
+			.withDockerFilePath(Paths.get("../../integration-tests/tci-webapp/Dockerfile"))
+			.withBaseDir(Paths.get("../../"))
+			.configureFilesToTransferHandler(h -> h
+				.withPostGitIgnoreLines(
+					// Ignore git-folder, as it will be provided in the Dockerfile
+					".git/**",
+					// Ignore other unused folders and extensions
+					"*.iml",
+					"*.cmd",
+					"*.md",
+					"_dev_infra/**",
+					"_resource_metrics/**",
+					// Ignore other Dockerfiles (our required file will always be transferred)
+					"Dockerfile",
+					// Ignore not required test-modules that may have changed
+					// sources only - otherwise the parent pom doesn't find the resources
+					"integration-tests/**",
+					"**/src/test/**",
+					// Ignore resources that are just used for development
+					"webapp/src/main/resources-dev/**",
+					// Most files from these folders need to be ignored -> Down there for highest prio
+					"node_modules",
+					"target")
+				// File is in root directory - we can't access it
+				.withBaseDirRelativeIgnoreFile(null)
+				.withDockerFileLinesModifier(new DockerfileCOPYParentsEmulator())
+				.withTransferArchiveTARCompressorCustomizer(c -> c
+					// Rewrite parent pom to exclude integration tests
+					// This way changes in test pom's cause no redownload of dependencies
+					.withContentModifier(new FileLinesContentModifier()
+					{
+						@Override
+						public boolean shouldApply(
+							final Path sourcePath,
+							final String targetPath,
+							final TarArchiveEntry tarArchiveEntry)
 						{
-							@Override
-							public boolean shouldApply(
-								final Path sourcePath,
-								final String targetPath,
-								final TarArchiveEntry tarArchiveEntry)
-							{
-								return "pom.xml".equals(targetPath);
-							}
-							
-							@Override
-							public List<String> modify(
-								final List<String> lines,
-								final Path sourcePath,
-								final String targetPath,
-								final TarArchiveEntry tarArchiveEntry)
-							{
-								return lines.stream()
-									// Remove integration tests module
-									.filter(s -> !s.contains("<module>integration-tests"))
-									.toList();
-							}
-							
-							@Override
-							public boolean isIdentical(final List<String> original, final List<String> created)
-							{
-								return original.size() == created.size();
-							}
-						}))
-				);
+							return "pom.xml".equals(targetPath);
+						}
+						
+						@Override
+						public List<String> modify(
+							final List<String> lines,
+							final Path sourcePath,
+							final String targetPath,
+							final TarArchiveEntry tarArchiveEntry)
+						{
+							return lines.stream()
+								// Remove integration tests module
+								.filter(s -> !s.contains("<module>integration-tests"))
+								.toList();
+						}
+						
+						@Override
+						public boolean isIdentical(final List<String> original, final List<String> created)
+						{
+							return original.size() == created.size();
+						}
+					}))
+			);
 		
 		if(JaCoCoConfig.instance().enabled())
 		{
